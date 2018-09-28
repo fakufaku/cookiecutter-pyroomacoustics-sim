a3c9b684a04878b912e3c22ffa2b37ccc5402b68
Robin Scheibler <fakufaku@gmail.com>
2018-09-28 11:11:54 +0900

    Two microphones. 3D is the default

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A cookie cutter to quickly get started with room acoustics simulation
 using [pyroomacoustics](https://github.com/LCAV/pyroomacoustics).
 
 This cookie cutter let's you create a basic scenario with one audio source
-and one microphone. Whether the simulation is 2D or 3D and the shape of the
+and two microphones. Whether the simulation is 2D or 3D and the shape of the
 room can be configured during the creation.
 
 ## Quick start
--- {{cookiecutter.script_name}}/{{cookiecutter.script_name}}.py
+++ {{cookiecutter.script_name}}/{{cookiecutter.script_name}}.py
@@ -12,21 +12,21 @@ max_order = {{ cookiecutter.max_order }}
 {%- if cookiecutter.dimension == "2D" %}
 room_dim = np.array([10, 7.5])
 source_loc = np.array([2.51, 3.57])
-mic_loc = np.c_[[7, 6.7]]
+mic_loc = np.c_[[7, 6.1],[6.9, 6.1]]
 {%- else %}
 room_dim = np.array([10, 7.5, 3])
 source_loc = np.array([2.51, 3.57, 1.7])
-mic_loc = np.c_[[7, 6.7, 1.3]]
+mic_loc = np.c_[[7, 6.1, 1.3],[6.9, 6.1, 1.3]]
 {%- endif %}
 {%- else %}
 corners = np.c_[[0,0], [0,4], [8,4], [8,2], [4,2], [4,0]]
 {%- if cookiecutter.dimension == "2D" %}
 source_loc = np.array([2.51, 2.57])
-mic_loc = np.c_[[6.7, 3.1]]
+mic_loc = np.c_[[6.7, 3.1], [6.1, 3]]
 {%- else %}
 height = 3
 source_loc = np.array([2.51, 2.57, 1.7])
-mic_loc = np.c_[[6.7, 3.1, 1.3]]
+mic_loc = np.c_[[6.7, 3.1, 1.3], [6.1, 3, 1.3]]
 {%- endif %}
 {%- endif %}
 
@@ -40,19 +40,39 @@ room.extrude(height)
 {%- endif %}
 {%- endif %}
 
-# Place the source
-room.add_source(source_loc)
+# Place a source of white noise playing for 5 s
+source_signal = np.random.randn(fs * 5)
+room.add_source(source_loc, signal=source_signal)
 
 # Place the microphone array
 room.add_microphone_array(
         pra.MicrophoneArray(mic_loc, fs=room.fs)
         )
 
-# Now the setup is finished, start doing whatever you like
-# As an example, we compute the RIR and plot a few images sources
-room.compute_rir()
-room.plot(img_order=4)
+# Now the setup is finished, run the simulation
+room.simulate()
 
+# As an example, we plot the simulated signals, the RIRs, and the room and a
+# few images sources
+
+# The microphone signal are in the rows of `room.mic_array.signals`
+mic_signals = room.mic_array.signals
+plt.figure()
+plt.subplot(1,2,1)
+plt.plot(np.arange(mic_signals.shape[1]) / fs, mic_signals[0])
+plt.title('Microphone 0 signal')
+plt.xlabel('Time [s]')
+plt.subplot(1,2,2)
+plt.plot(np.arange(mic_signals.shape[1]) / fs, mic_signals[1])
+plt.title('Microphone 1 signal')
+plt.xlabel('Time [s]')
+plt.tight_layout()
+
+# Plot the room and the image sources
+room.plot(img_order=4)
+plt.title('The room with 4 generations of image sources')
+
+# Plot the impulse responses
 plt.figure()
 room.plot_rir()
 
